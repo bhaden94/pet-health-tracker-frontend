c2f114cc48487ce6706ce9975b44ff0d611cdebb
bhaden94 <bhaden94@yahoo.com>
2020-07-28 14:33:48 -0700

    form submit for new workout handled

--- src/components/AddModifyForm.js
+++ src/components/AddModifyForm.js
@@ -1,165 +1,213 @@
 import React, { useState, useEffect } from "react";
-import { Typography, Slider, TextField, Button, Icon } from "@material-ui/core";
 import {
-  MuiPickersUtilsProvider,
-  KeyboardDateTimePicker,
+    Typography,
+    Slider,
+    TextField,
+    Button,
+    Icon,
+    MenuItem,
+} from "@material-ui/core";
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDateTimePicker,
 } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
-import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const AddModifyForm = ({ data }) => {
-  const [id, setId] = useState();
-  const [type, setType] = useState("Walk");
-  const [dateTime, setDateTime] = useState(new Date());
-  const [intensity, setIntensity] = useState(1);
-  const [distance, setDistance] = useState("0.0");
-  const [pets, setPets] = useState([]);
-  const [chosenPet, setChosenPet] = useState();
-  const [calories, setCalories] = useState("0");
-  const [isLoading, setIsLoading] = useState(false);
+    const [id, setId] = useState();
+    const [type, setType] = useState("Walk");
+    const [dateTime, setDateTime] = useState(new Date());
+    const [intensity, setIntensity] = useState(1);
+    const [distance, setDistance] = useState();
+    const [pets, setPets] = useState([]);
+    const [chosenPet, setChosenPet] = useState();
+    const [calories, setCalories] = useState();
+    const [duration, setDuration] = useState();
 
-  // marks for slider component
-  const marks = [
-    {
-      value: 1,
-      label: "Low",
-    },
-    {
-      value: 2,
-      label: "Medium",
-    },
-    {
-      value: 3,
-      label: "High",
-    },
-  ];
+    const [btnTxt, setBtnTxt] = useState("Submit");
+    const [isLoading, setIsLoading] = useState(false);
 
-  const typeProps = {
-    options: ["Walk", "Run", "Other"],
-  };
+    // marks for slider component
+    const marks = [
+        {
+            value: 1,
+            label: "Low",
+        },
+        {
+            value: 2,
+            label: "Medium",
+        },
+        {
+            value: 3,
+            label: "High",
+        },
+    ];
 
-  const petsProps = {
-    options: pets,
-    getOptionLabel: (option) => option.name,
-  };
+    // type of workouts allowed from user
+    const types = ["Walk", "Run", "Other"];
 
-  useEffect(() => {
-    async function fetchPets() {
-      setIsLoading(true);
-      const res = await fetch("/api/pets");
-      const json = await res.json();
-      setPets(json);
-      setIsLoading(false);
-    }
-    fetchPets();
-    if (data) {
-      setId(data.id);
-      setType(data.type);
-      setDateTime(data.dateTime);
-      setIntensity(data.intensity);
-      setDistance(data.distance);
-      setChosenPet(data.chosenPet);
-      setCalories(data.calories);
-    }
-  }, [data]);
+    const petsProps = {
+        options: pets,
+        getOptionLabel: (option) => option.name,
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(type, dateTime, intensity, distance, chosenPet, calories);
-  };
+    useEffect(() => {
+        async function fetchPets() {
+            setIsLoading(true);
+            const res = await fetch("/api/pets");
+            const json = await res.json();
+            setPets(json);
+            setIsLoading(false);
+        }
+        fetchPets();
+        if (data) {
+            setId(data.id);
+            setType(data.type);
+            setDateTime(data.dateTime);
+            setIntensity(data.intensity);
+            setDistance(data.distance);
+            setChosenPet(data.chosenPet);
+            setCalories(data.calories);
+            setDuration(data.duration);
+            setBtnTxt("Update");
+        }
+    }, [data]);
 
-  return (
-    <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <Autocomplete
-            style={{ width: "50%", paddingLeft: "25vw" }}
-            {...typeProps}
-            id="open-on-focus"
-            value={type}
-            openOnFocus
-            renderInput={(params) => (
-              <TextField {...params} label="Workout Type" margin="normal" />
-            )}
-            onChange={(e) => setType(e.target.innerHTML)}
-          />
-          <br />
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <KeyboardDateTimePicker
-              style={{ width: "50%" }}
-              variant="inline"
-              ampm={false}
-              label="Date & Time"
-              value={dateTime}
-              format="yyyy/MM/dd hh:mm a"
-              onChange={(e) => setDateTime(e)}
-            />
-          </MuiPickersUtilsProvider>
-          <br />
-          <div>
-            <Typography id="discrete-slider" gutterBottom>
-              Workout Intensity
-            </Typography>
-            <Slider
-              style={{ width: "50%" }}
-              value={intensity}
-              aria-label="discrete-slider"
-              step={1}
-              marks={marks}
-              min={1}
-              max={3}
-              onChange={(e, value) => setIntensity(value)}
-            />
-          </div>
-          <br />
-          <TextField
-            style={{ width: "50%" }}
-            type="number"
-            value={distance}
-            inputProps={{ step: ".1", min: "0" }}
-            label="Distance in Miles"
-            onChange={(e) => setDistance(e.target.value)}
-          />
-          <br />
-          <Autocomplete
-            style={{ width: "50%", paddingLeft: "25vw" }}
-            {...petsProps}
-            id="open-on-focus"
-            value={chosenPet}
-            openOnFocus
-            renderInput={(params) => (
-              <TextField {...params} label="Pet" margin="normal" />
-            )}
-            onChange={(e) =>
-              setChosenPet(
-                pets.filter((pet) => pet.name === e.target.innerHTML)[0]
-              )
-            }
-          />
-          <TextField
-            style={{ width: "50%" }}
-            type="number"
-            value={calories}
-            inputProps={{ step: "1", min: "0" }}
-            label="Calories Burned"
-            onChange={(e) => setCalories(e.target.value)}
-          />
-          <br />
-          <Button
-            style={{ width: "50%", marginTop: "30px" }}
-            variant="contained"
-            color="primary"
-            endIcon={<Icon>send</Icon>}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </form>
-      )}
-    </div>
-  );
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (data) {
+            console.log("put request would go here");
+        } else {
+            const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    type: type,
+                    date_time: dateTime,
+                    intensity: intensity,
+                    calories_burned: calories,
+                    distance: distance,
+                    pet: chosenPet,
+                    workout_duration: duration,
+                }),
+            };
+            const res = await fetch("/api/add_workout", requestOptions);
+            const json = await res.json();
+            console.log(json);
+        }
+    };
+
+    return (
+        <div>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                    <form onSubmit={handleSubmit} autoComplete="off">
+                        <TextField
+                            style={{ width: "50%", textAlign: "left" }}
+                            select
+                            value={type}
+                            label="Workout Type"
+                            required
+                            onChange={(e) => setType(e.target.value)}
+                            margin="normal"
+                        >
+                            {types.map((option, index) => (
+                                <MenuItem key={index} value={option}>
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <br />
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <KeyboardDateTimePicker
+                                style={{ width: "50%" }}
+                                variant="inline"
+                                ampm={false}
+                                label="Date & Time"
+                                value={dateTime}
+                                format="yyyy/MM/dd hh:mm a"
+                                onChange={(e) => setDateTime(e)}
+                            />
+                        </MuiPickersUtilsProvider>
+                        <br />
+                        <div>
+                            <Typography id="discrete-slider" gutterBottom>
+                                Workout Intensity
+                            </Typography>
+                            <Slider
+                                style={{ width: "50%" }}
+                                value={intensity}
+                                aria-label="discrete-slider"
+                                step={1}
+                                marks={marks}
+                                min={1}
+                                max={3}
+                                onChange={(e, value) => setIntensity(value)}
+                            />
+                        </div>
+                        <br />
+                        <TextField
+                            style={{ width: "50%" }}
+                            type="number"
+                            value={distance}
+                            inputProps={{ step: ".1", min: "0" }}
+                            label="Distance in Miles"
+                            onChange={(e) => setDistance(e.target.value)}
+                        />
+                        <br />
+                        <TextField
+                            style={{ width: "50%" }}
+                            type="number"
+                            value={duration}
+                            required
+                            inputProps={{ step: "1", min: "0" }}
+                            label="Workout Duration in Minutes"
+                            onChange={(e) => setDuration(e.target.value)}
+                        />
+                        <br />
+                        <TextField
+                            style={{ width: "50%", textAlign: "left" }}
+                            select
+                            value={chosenPet}
+                            label="Pet"
+                            required
+                            onChange={(e) =>
+                                setChosenPet(
+                                    pets.filter((pet) => pet.name === e.target.value)[0]
+                                )
+                            }
+                            margin="normal"
+                        >
+                            {pets.map((option, index) => (
+                                <MenuItem key={index} value={option.name}>
+                                    {option.name}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <br />
+                        <TextField
+                            style={{ width: "50%" }}
+                            type="number"
+                            value={calories}
+                            inputProps={{ step: "1", min: "0" }}
+                            label="Calories Burned"
+                            onChange={(e) => setCalories(e.target.value)}
+                        />
+                        <br />
+                        <Button
+                            style={{ width: "50%", marginTop: "30px" }}
+                            variant="contained"
+                            color="primary"
+                            endIcon={<Icon>send</Icon>}
+                            type="submit"
+                        >
+                            {btnTxt}
+                        </Button>
+                    </form>
+                )}
+        </div>
+    );
 };
 
 export default AddModifyForm;
