ae260093f134b4a1385505cb4df2498995aa03d5
bhaden94 <bhaden94@yahoo.com>
2020-07-29 11:59:03 -0700

    responsiveness added to table and form. success and failure alerts also added to table

--- src/components/AddModifyForm.js
+++ src/components/AddModifyForm.js
@@ -9,12 +9,14 @@ import {
     MenuItem,
     ListItemIcon,
     ListItemText,
-    CircularProgress
+    CircularProgress,
+    Snackbar,
 } from "@material-ui/core";
 import {
     MuiPickersUtilsProvider,
     KeyboardDateTimePicker,
 } from "@material-ui/pickers";
+import Alert from "@material-ui/lab/Alert";
 import DateFnsUtils from "@date-io/date-fns";
 
 const AddModifyForm = (props) => {
@@ -29,9 +31,11 @@ const AddModifyForm = (props) => {
     const [duration, setDuration] = useState();
 
     const [btnHeadingTxt, setBtnHeadingTxt] = useState("Add");
-    const [btnIcon, setBtnIcon] = useState("add")
+    const [btnIcon, setBtnIcon] = useState("add");
     const [isLoading, setIsLoading] = useState(false);
-    const [postLoading, setPostLoading] = useState(false)
+    const [postLoading, setPostLoading] = useState(false);
+    const [showSuccess, setShowSucess] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
 
     const location = useLocation();
 
@@ -70,18 +74,41 @@ const AddModifyForm = (props) => {
             setDateTime(location.state.date_time);
             setIntensity(location.state.intensity);
             setDistance(location.state.distance);
-            setChosenPet(location.state.pet)
+            setChosenPet(location.state.pet);
             setCalories(location.state.calories_burned);
             setDuration(location.state.workout_duration);
             setBtnHeadingTxt("Update");
-            setBtnIcon("update")
+            setBtnIcon("update");
         }
     }, [props]);
 
     const handleSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        setShowSucess(false);
+        setShowAlert(false);
         if (btnHeadingTxt === "Update") {
-            console.log("put request would go here");
+            const requestOptions = {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    type: type,
+                    date_time: dateTime,
+                    intensity: intensity,
+                    calories_burned: calories,
+                    distance: distance,
+                    pet: chosenPet,
+                    workout_duration: duration,
+                }),
+            };
+            setPostLoading(true);
+            const res = await fetch(`/api/workouts/${id}`, requestOptions);
+            const json = await res.json();
+            if (json.id) {
+                setShowSucess(true);
+            } else {
+                setShowAlert(true);
+            }
+            setPostLoading(false);
         } else {
             const requestOptions = {
                 method: "POST",
@@ -96,146 +123,167 @@ const AddModifyForm = (props) => {
                     workout_duration: duration,
                 }),
             };
-            setPostLoading(true)
+            setPostLoading(true);
             const res = await fetch("/api/add_workout", requestOptions);
             const json = await res.json();
-            setPostLoading(false)
+            if (json.id) {
+                setShowSucess(true);
+            } else {
+                setShowAlert(true);
+            }
+            setPostLoading(false);
         }
     };
 
+    const snackBarClose = (e, reason) => {
+        if (reason === 'clickaway') {
+            return
+        }
+        setShowSucess(false)
+        setShowAlert(false)
+    }
+
     return (
         <div className="add-modify-form">
+            <h1>{btnHeadingTxt} Workout</h1>
             {isLoading ? (
-                <p>Loading...</p>
+                <CircularProgress size={50} color="primary" />
             ) : (
-                    <div>
-                        <h1>{btnHeadingTxt} Workout</h1>
-                        <form className="form" onSubmit={handleSubmit} autoComplete="off">
-                            <div className="form-item">
-                                <TextField
-                                    className="pet-input"
-                                    select
-                                    value={chosenPet ? chosenPet.name : ""}
-                                    label="Pet"
-                                    required
-                                    onChange={(e) =>
-                                        setChosenPet(
-                                            pets.filter((pet) => pet.name === e.target.value)[0]
-                                        )
-                                    }
-                                    margin="normal"
-                                >
-                                    {pets.map((option, index) => (
-                                        <MenuItem key={index} value={option.name}>
-                                            <ListItemText primary={option.name} />
-                                            <ListItemIcon>
-                                                <img
-                                                    style={{ width: "40px" }}
-                                                    alt="dog"
-                                                    src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"
-                                                />
-                                            </ListItemIcon>
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                            </div>
-                            <div className="form-item">
-                                <TextField
-                                    className="type-input"
-                                    select
-                                    value={type}
-                                    label="Workout Type"
-                                    required
-                                    onChange={(e) => setType(e.target.value)}
-                                    margin="normal"
-                                >
-                                    {types.map((option, index) => (
-                                        <MenuItem key={index} value={option}>
-                                            <ListItemText primary={option} />
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                            </div>
-                            <br />
-                            <div className="form-item">
-                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                    <KeyboardDateTimePicker
-                                        className="date-time-input"
-                                        variant="inline"
-                                        ampm={false}
-                                        label="Date & Time"
-                                        value={dateTime}
-                                        format="yyyy/MM/dd hh:mm a"
-                                        onChange={(e) => setDateTime(e)}
-                                    />
-                                </MuiPickersUtilsProvider>
-                            </div>
-                            <br />
-                            <div className="form-item">
-                                <Typography className="slider-input">
-                                    Workout Intensity
-                                </Typography>
-                                <Slider
-                                    className="form-slider"
-                                    value={intensity}
-                                    step={1}
-                                    marks={marks}
-                                    min={1}
-                                    max={3}
-                                    onChange={(e, value) => setIntensity(value)}
+                    <form className="form" onSubmit={handleSubmit} autoComplete="off">
+                        <div className="form-item">
+                            <TextField
+                                className="pet-input"
+                                select
+                                value={chosenPet ? chosenPet.name : ""}
+                                label="Pet"
+                                required
+                                onChange={(e) =>
+                                    setChosenPet(
+                                        pets.filter((pet) => pet.name === e.target.value)[0]
+                                    )
+                                }
+                                margin="normal"
+                            >
+                                {pets.map((option, index) => (
+                                    <MenuItem key={index} value={option.name}>
+                                        <ListItemText primary={option.name} />
+                                        <ListItemIcon>
+                                            <img
+                                                style={{ width: "40px" }}
+                                                alt="dog"
+                                                src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"
+                                            />
+                                        </ListItemIcon>
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        </div>
+                        <div className="form-item">
+                            <TextField
+                                className="type-input"
+                                select
+                                value={type}
+                                label="Workout Type"
+                                required
+                                onChange={(e) => setType(e.target.value)}
+                                margin="normal"
+                            >
+                                {types.map((option, index) => (
+                                    <MenuItem key={index} value={option}>
+                                        <ListItemText primary={option} />
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        </div>
+                        <br />
+                        <div className="form-item">
+                            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <KeyboardDateTimePicker
+                                    className="date-time-input"
+                                    variant="inline"
+                                    ampm={false}
+                                    label="Date & Time"
+                                    value={dateTime}
+                                    format="yyyy/MM/dd hh:mm a"
+                                    onChange={(e) => setDateTime(e)}
                                 />
-                            </div>
-                            <br />
-                            <div className="distance-input form-item">
-                                <TextField
-                                    type="number"
-                                    value={distance}
-                                    inputProps={{ step: ".1", min: "0" }}
-                                    label="Distance in Miles"
-                                    onChange={(e) => setDistance(e.target.value)}
-                                />
-                            </div>
-                            <br />
-                            <div className="duration-input form-item">
-                                <TextField
-                                    type="number"
-                                    value={duration}
-                                    required
-                                    inputProps={{ step: "1", min: "0" }}
-                                    label="Workout Duration in Minutes"
-                                    onChange={(e) => setDuration(e.target.value)}
-                                />
-                            </div>
-                            <br />
+                            </MuiPickersUtilsProvider>
+                        </div>
+                        <br />
+                        <div className="form-item">
+                            <Typography className="slider-input">Workout Intensity</Typography>
+                            <Slider
+                                className="form-slider"
+                                value={intensity}
+                                step={1}
+                                marks={marks}
+                                min={1}
+                                max={3}
+                                onChange={(e, value) => setIntensity(value)}
+                            />
+                        </div>
+                        <br />
+                        <div className="distance-input form-item">
+                            <TextField
+                                type="number"
+                                value={distance}
+                                inputProps={{ step: ".1", min: "0" }}
+                                label="Distance in Miles"
+                                onChange={(e) => setDistance(e.target.value)}
+                            />
+                        </div>
+                        <br />
+                        <div className="duration-input form-item">
+                            <TextField
+                                type="number"
+                                value={duration}
+                                required
+                                inputProps={{ step: "1", min: "0" }}
+                                label="Workout Duration in Minutes"
+                                onChange={(e) => setDuration(e.target.value)}
+                            />
+                        </div>
+                        <br />
 
-                            <br />
-                            <div className="calorie-input form-item">
-                                <TextField
-                                    type="number"
-                                    value={calories}
-                                    inputProps={{ step: "1", min: "0" }}
-                                    label="Calories Burned"
-                                    onChange={(e) => setCalories(e.target.value)}
-                                />
-                            </div>
-                            <br />
-                            <div className="form-item submit">
-                                <Button
-                                    className="submit-btn"
-                                    variant="contained"
+                        <br />
+                        <div className="calorie-input form-item">
+                            <TextField
+                                type="number"
+                                value={calories}
+                                inputProps={{ step: "1", min: "0" }}
+                                label="Calories Burned"
+                                onChange={(e) => setCalories(e.target.value)}
+                            />
+                        </div>
+                        <br />
+                        <div className="form-item submit">
+                            <Button
+                                className="submit-btn"
+                                variant="contained"
+                                color="primary"
+                                size="large"
+                                disabled={postLoading}
+                                endIcon={<Icon>{btnIcon}</Icon>}
+                                type="submit"
+                            >
+                                {btnHeadingTxt}
+                            </Button>
+                            {postLoading && (
+                                <CircularProgress
+                                    size={24}
                                     color="primary"
-                                    size="large"
-                                    disabled={postLoading}
-                                    endIcon={<Icon>{btnIcon}</Icon>}
-                                    type="submit"
-                                >
-                                    {btnHeadingTxt}
-                                </Button>
-                                {postLoading && <CircularProgress size={24} color="primary" className="post-loading" />}
-                            </div>
-                        </form>
-                    </div>
+                                    className="post-loading"
+                                />
+                            )}
+                        </div>
+                    </form>
                 )}
+            <Snackbar open={showSuccess} autoHideDuration={5000} onClose={snackBarClose}>
+                <Alert onClose={snackBarClose} severity="success">Sucess!</Alert>
+            </Snackbar>
+            <Snackbar open={showAlert} autoHideDuration={5000} onClose={snackBarClose}>
+                <Alert onClose={snackBarClose} severity="error">Something went wrong, please try again</Alert>
+            </Snackbar>
         </div>
     );
 };
--- src/components/WorkoutTable.js
+++ src/components/WorkoutTable.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 import MaterialTable from "material-table";
 
-const WorkoutTable = ({history}) => {
+const WorkoutTable = ({ history }) => {
     const [workouts, setWorkouts] = useState([])
 
     const [isLoading, setIsLoading] = useState(false)
+    const [showSuccess, setShowSucess] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
 
 
     const columns = [
         {
             title: "Date & Time",
-            field: "date_time"
+            field: "date_time",
+            render: rowData => new Date(rowData.date_time).toLocaleString()
         },
         {
             title: "Pet Name",
@@ -32,12 +38,21 @@ const WorkoutTable = ({history}) => {
         console.log(rowData)
         history.push({
             pathname: '/add_modify',
-            state: {...rowData}
+            state: { ...rowData }
         })
     }
 
-    const deleteWorkout = (e, rowData) => {
-        console.log(rowData.id)
+    const deleteWorkout = async (e, rowData) => {
+        const requestOptions = {
+            method: "DELETE"
+        };
+        const res = await fetch(`/api/remove_workout/${rowData.id}`, requestOptions);
+        fetchWorkouts()
+        if(res.status === 200) {
+            setShowSucess(true)
+        } else {
+            setShowAlert(true)
+        }
     }
 
     const actions = [
@@ -52,30 +67,45 @@ const WorkoutTable = ({history}) => {
             onClick: deleteWorkout
         }
     ]
+    async function fetchWorkouts() {
+        setIsLoading(true);
+        const res = await fetch("/api/workouts");
+        const json = await res.json();
+        setWorkouts(json);
+        setIsLoading(false);
+    }
 
     useEffect(() => {
-        async function fetchWorkouts() {
-            setIsLoading(true);
-            const res = await fetch("/api/workouts");
-            const json = await res.json();
-            console.log(json)
-            setWorkouts(json);
-            setIsLoading(false);
-        }
         fetchWorkouts();
     }, []);
 
-    return ( 
+    const snackBarClose = (e, reason) => {
+        if (reason === 'clickaway') {
+            return
+        }
+        setShowSucess(false)
+        setShowAlert(false)
+    }
+
+    return (
         <div className="workout-table">
-        <h1>Workouts</h1>
-            <MaterialTable
-            columns={columns}
-            data={workouts}
-            actions={actions}
-            title="Workouts"
-            />
-      </div>
-     );
+            <h1>Workouts</h1>
+            {isLoading ? <CircularProgress size={50} color="primary" /> :
+                <MaterialTable
+                    columns={columns}
+                    data={workouts}
+                    actions={actions}
+                    title="Workouts"
+                />
+            }
+            <Snackbar open={showSuccess} autoHideDuration={5000} onClose={snackBarClose}>
+                <Alert onClose={snackBarClose} severity="success">Sucess!</Alert>
+            </Snackbar>
+            <Snackbar open={showAlert} autoHideDuration={5000} onClose={snackBarClose}>
+                <Alert onClose={snackBarClose} severity="error">Something went wrong, please try again</Alert>
+            </Snackbar>
+        </div>
+    );
 }
- 
+
 export default withRouter(WorkoutTable);
